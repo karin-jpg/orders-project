ca85d71db774b2ad7fe06cbdd7aacd69c4578778
karin morais <andradkarin@gmail.com>
2024-06-20 21:51:10 -0300

    fixed cors error

--- public/index.php
+++ public/index.php
@@ -12,13 +12,14 @@ if ($_SERVER['APP_DEBUG']) {
     Debug::enable();
 }
 
-if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
-    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO);
-}
+// if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
+//     Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO);
+// }
 
-if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
-    Request::setTrustedHosts([$trustedHosts]);
-}
+// if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
+//     Request::setTrustedHosts([$trustedHosts]);
+// }
+
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
